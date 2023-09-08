b6dff6af3bf0b61f1377f48e0c2b2bcdd0de228d
Ashish Padhi <asis_kumar2004@yahoo.com>
2023-09-08 15:00:27 +0530

    removed another commet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,5 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-// unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
